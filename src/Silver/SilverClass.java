ed31f519289536030a425483d55852e7b7ddf763
maru7500 <appleapple7500>
2020-04-22 19:11:24 +0900

    Initial commit

---
+++ src/Silver/SilverClass.java
@@ -0,0 +1,18 @@
+package Silver;
+
+public class SilverClass {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		char[] c;
+		c = new char[3];
+		int[] i = new int[3];
+		String str[] = {"hey","java","!!"};
+		c[0] = 'A'; c[1] = 'B'; c[2] = 'C';
+		i[0] = 1; i[1] = 10; i[2] = 200;
+		System.out.println("str-size :" + str.length);
+		System.out.println("c[0] :" + c[0]);
+		System.out.println("i[2] :" + i[2]);
+	}
+
+}
