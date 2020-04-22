2df80ca2ff7b1242f1a503228290c76127ec1f48
maru7500 <appleapple7500>
2020-04-22 19:21:47 +0900

    mary_array

--- src/Silver/SilverClass.java
+++ src/Silver/SilverClass.java
@@ -4,15 +4,12 @@ public class SilverClass {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		char[] c;
-		c = new char[3];
-		int[] i = new int[3];
-		String str[] = {"hey","java","!!"};
-		c[0] = 'A'; c[1] = 'B'; c[2] = 'C';
-		i[0] = 1; i[1] = 10; i[2] = 200;
-		System.out.println("str-size :" + str.length);
-		System.out.println("c[0] :" + c[0]);
-		System.out.println("i[2] :" + i[2]);
+		int [][] array;
+		array = new int[3][4];
+		array[0][0] = 100;
+		array[0][1] = 200;
+		System.out.println(array[0][0]);
+		System.out.println(array[0][1]);
 	}
 
 }
